f029a5102db76aab29ab6067b1a1ff1dfa333038
Pamela Chong <pamelachong@Pamelas-MacBook-Air.local>
2018-08-01 14:58:13 -0700

    Adding basic React setup

---
+++ mafia/imports/ui/containers/App/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+import "./styles.css";
+import PropTypes from "prop-types";
+
+class App extends Component {
+    render(){
+        return (
+            <h1> Hello Township </h1>
+        )
+    }
+}
+
+
+export default (App);
