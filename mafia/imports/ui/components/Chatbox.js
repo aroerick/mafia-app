632854f8591bac8d30f03c4bc6a7b6485874b533
aroerick <a.roerick@gmail.com>
2018-08-07 08:46:36 -0700

    Wrap inputs with Semantic UI.

--- mafia/imports/ui/components/Chatbox.js
+++ mafia/imports/ui/components/Chatbox.js
@@ -1,34 +1,31 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import { Message } from 'semantic-ui-react';
+import PropTypes from "prop-types";
+import { Message } from "semantic-ui-react";
 
 const Chatbox = ({ messages }) => (
-
-  <div style={{maxHeight: '400px', overflowY:'scroll'}}>
-    {messages.map(message => 
-    <Message color={(message.sender === 'Narrator') ? "olive" : "grey"}floating key={message._id}><Message.Header style={{fontWeight: 600}}>{message.sender} to {message.recipient}:</Message.Header>
-    {/* <span>  */}
-    {message.text} 
-    {/* <input type="checkbox" 
-    id={todo._id} checked={todo.completed}
-     onChange = {() => handleCheckbox(todo)}/>
-    <label htmlFor={todo._id} />
-
-    <button onClick = {()=>handleDeleteTodo(todo)} >
-      <i className="fa fa-trash" /> */}
-    {/* </button> */}
-    {/* </span> */}
-    </Message>)}
+  <div style={{ maxHeight: "400px", overflowY: "scroll" }}>
+    {messages.map(message => (
+      <Message
+        color={message.sender === "Narrator" ? "olive" : "grey"}
+        floating
+        key={message._id}
+      >
+        <Message.Header style={{ fontWeight: 600 }}>
+          {message.sender} to {message.recipient}:
+        </Message.Header>
+        {message.text}
+      </Message>
+    ))}
   </div>
 );
 
 Chatbox.propTypes = {
-    messages: PropTypes.arrayOf(
-        PropTypes.shape({
-            _id: PropTypes.string.isRequired,
-            text: PropTypes.string.isRequired,
-        })
-    )
-}
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired
+    })
+  )
+};
 
 export default Chatbox;
