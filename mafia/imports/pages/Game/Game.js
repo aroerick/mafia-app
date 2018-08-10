1c03cea30f949f69735ee826faaab2296e41f2d8
aroerick <a.roerick@gmail.com>
2018-08-10 09:22:42 -0700

    Additional styling on buttons.

--- mafia/imports/pages/Game/Game.js
+++ mafia/imports/pages/Game/Game.js
@@ -181,7 +181,6 @@ class Game extends Component {
         {Mafia.find({ player: currentUserId }).count() === 0 ? (
           <Form error={this.state.joinGameError}>
             <Form.Field>
-							<Icon name="plus" />
               <input
                 type="text"
                 placeholder="What's your name?"
--- mafia/imports/ui/components/PlayerCard.js
+++ mafia/imports/ui/components/PlayerCard.js
@@ -4,26 +4,36 @@ import { Button, Header, Image, Modal, Icon } from 'semantic-ui-react'
 const PlayerCard = ({ currentUser }) => {
   text = "";
   roleTitle = "";
+  roleName = "";
+  color = "";
   switch (currentUser[0].role) {
     case "mafia":
       text = "You pick your victims with your teammate. When the night falls, you have the choice to lynch whoever is you decide to eleminate."
       roleTitle = "a Mafioso"
+      roleName = "Mafia"
+      color = "red"
       break;
     case "doctor":
       text = "Try to save the victim who is about to die. And be careful to be the victim yourself!. The more you save lives the more you live!."
       roleTitle = "the Doctor"
+      roleName = "Doctor"
+      color = "teal"
       break;
     case "detective":
       text = "Be smart and guess the mafias. Help civilians to live and kill the mafias."
       roleTitle = "the Detective"
+      roleName = "Detective"
+      color = "yellow"
       break;
     default:
       text = "When the night falls, pray not to be the victim . But if you guessed the mafias when the day comes you will be alive longer!."
       roleTitle = "a Civilian"
+      roleName = "Civilian"
+      color = "purple"
   }
   return (
   <Modal trigger={
-    <Button basic color="purple" icon labelPosition="right">
+    <Button fluid basic color={color} icon labelPosition="right" style={{ marginBottom: 15 }}>
       {currentUser[0].name}, you are {roleTitle}
       <Icon name="align left" />
     </Button>}>
@@ -32,7 +42,7 @@ const PlayerCard = ({ currentUser }) => {
       <Image wrapped size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' />
       <Modal.Description>
         <Header>
-          {currentUser[0].role}
+          {roleName}
         </Header>
           <p>{text}</p>
       </Modal.Description>
